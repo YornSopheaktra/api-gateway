5dd043ddeeecf37708d1940d80c6a7df1bb3fb58
Sopheaktra <sopheaktra.yorn@hatthabank.com>
2025-02-04 23:57:09 +0700

    init

---
+++ src/main/java/com/spt/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.spt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api-gateway")
+public class AdminController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome to API Gateway!";
+    }
+}
